9a5ad83044785bcf17745c296a1b1851706a8b2b
Peng LEI <peng.lei@n-hop.com>
2023-11-09 15:33:19 +0800

    Update GitHub CI workflow

--- cmake/arm_gcc10.2.cmake
+++ cmake/arm_gcc10.2.cmake
@@ -1,7 +1,7 @@
 #
 # EXAMPLE CMAKE FILE FOR CROSS COMPILING
 #
-set(TOOLCHAIN_DIR "/usr/local/arm_gcc10.2_toolchain/")
+option(TOOLCHAIN_DIR "/usr/local/arm_gcc10.2_toolchain/")
 # (1) tools from
 # https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.2-2020.11/binrel/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu.tar.xz
 # (2) install binaries to /usr/local/arm_gcc10.2_toolchain/
@@ -12,6 +12,7 @@ set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_DIR})
 set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-g++)
+message(STATUS "TOOLCHAIN PATH: ${TOOLCHAIN_DIR}")
 message(STATUS "ROOT PATH: ${CMAKE_FIND_ROOT_PATH}")
 
 # search for programs in the build host directories
